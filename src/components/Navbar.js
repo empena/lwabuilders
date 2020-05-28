70cf9b6e63655e54547402a083fdf68eada20602
Emily Pena <emilyapena@gmail.com>
2020-05-27 22:55:54 -0600

    navabr feature

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="mainNav">
+        <div className="leftNavStyles">
+          <img className="logo" width="65" height="30" src="https://res.cloudinary.com/dvwwzmsmn/image/upload/v1590618404/LWA/LOGO_NAME_white_xklb9c.png" />
+
+        </div>
+        <div className="rightNavStyles">
+          <a href="#services" className="navbutton">
+            SERVICES
+          </a>
+          <a href="#reviews" className="navbutton">
+            REVIEWS
+          </a>
+          <a href="#contact" className="navbuttonOne">
+            CONTACT
+          </a>
+          <a href="#contact" className="navbuttonTwo">
+            GET A QUOTE
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
